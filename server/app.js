3b42b61456b6be4b141bd6ae9b192bc028ee483e
Devin Jackson <devin@Devins-MBP.home>
2019-10-06 18:58:17 -0400

    connected graphql schema to express

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const graphqlHTTP = require("express-graphql")
+const schema = require('./schema/schema')
 
 const app = express()
 
-app.use('/graphql', graphqlHTTP({}))
+app.use('/graphql', graphqlHTTP({
+    schema
+}))
 
 app.listen(4000, () => {
     console.log("now listening on port 4000")
