6005789e19e71d5cf70804d1e3bc31ab9bd41779
Devin Jackson <devin@Devins-MBP.home>
2019-10-06 19:04:24 -0400

    finished resolve function fn for finding books by ID

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+
+const books = [
+    {name : 'Harry Potter', genre: 'Fantasy', id: '1'},
+    {name : 'Eragon', genre: 'Fantasy', id: '2'},
+    {name : 'Drizzt', genre: 'Fantasy', id: '3'},
+];
 
 const BookType = GraphQLObjectType({
     name: 'Book',
@@ -18,7 +26,7 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: {id: {type: GraphQLString}},
             resolve(parent, args){
-
+                _.find(books, {id: args.id})
             }
         }
     }
