7c8854a21f1efc7c6801e82353bbed5bcd50927a
Devin Jackson <devin@Devins-MBP.home>
2019-10-16 22:10:22 -0400

    mongoose connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require("express-graphql")
 const schema = require('./schema/schema')
+const mongoose = require('mongoose');
 
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/graphql-tutorial')
+mongoose.connection.once('open', ()=>{ 
+    console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
